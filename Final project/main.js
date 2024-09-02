8ec64fe1473ebc2052addadbc9fe47040d910dc0
mazenIsmeal <mazenIsmeal309@gmail.com>
2024-09-02 18:07:47 +0300

    update

--- Final project/main.js
+++ Final project/main.js
@@ -13,30 +13,107 @@ closeAdd.addEventListener('click', function() {
 // ----------------- Function Open Popup Add --------------------
 
 // ----------------- Function Open Popup Update --------------------
-let openPopuptUpdate = document.querySelector('#openPopuptUpdate');
 let popup_update = document.querySelector('.popup_update');
 let closeUpdate = document.querySelector('#closeUpdate')
 
-openPopuptUpdate.addEventListener('click', function() {
-    popup_update.style.display = 'block'
-})
+// function openPopupUpdate() {
+//     popup_update.style.display = 'block'
+// }
 
 closeUpdate.addEventListener('click', function() {
     popup_update.style.display = 'none'
 })
 // ----------------- Function Open Popup Update --------------------
 
-// ----------------- Function Create Task --------------------
-// let addTask = document.querySelector('.addTask');
-// let send = document.querySelector('.send');
-// let addTaskArray = [];
-// let list = document.querySelector('.list')
+// ----------------- Function Read Task --------------------
+let list = document.querySelector('.pList')
+let tasks = [
+    {
+        'title': 'mazen',
+        'date': '1/9/2024',
+        'isDone': false
+    },
+    {
+        'title': 'ashraf',
+        'date': '1/9/2024',
+        'isDone': false
+    },
+    {
+        'title': 'ismeal',
+        'date': '1/9/2024',
+        'isDone': false
+    }
+]
+function readTask() {
+    let index = 0;
+    for(task of tasks) {
+        let readTask = `
+            <div class='list'>
+                <div class="title_list">
+                    <h2>${task.title}</h2>
+                    <i class="fa-solid fa-calendar-days"></i>
+                    <span>${task.date}</span>
+                </div>
+                <div class="icon">
+                    <i class="fa-solid fa-trash" style="background-color: brown;" onclick='deleteTask(${index})'></i>
+                    <i class="fa-solid fa-check" style="background-color: green;"></i>
+                    <i class="fa-solid fa-pen" style="background-color: blue;" onclick='openPopupUpdate(${index})' id="openPopuptUpdate"></i>
+                </div>
+            </div>
+        `
+        list.innerHTML += readTask
+        index++
+    }
+}
+readTask()
+// ----------------- Function Read Task --------------------
 
-// send.addEventListener('click', function() {
-//     addTaskArray.push(addTask.value)
-//     for(let i = 0; i < addTaskArray.length; i++) {
-//         let createTask = document.createElement('div');
-//         createTask.textContent = addTaskArray[0]
-//         list.appendChild(createTask)   
-//     }
-// })
+/* _________________ Function Create Task ___________________ */
+let submit = document.querySelector('.send');
+
+submit.addEventListener('click', function() {
+    list.innerHTML = ''
+    let addTask = document.querySelector('.addTask').value;
+    let now = new Date();
+    let date = `${now.getDate()}/ ${now.getMonth() + 1}/ ${now.getFullYear()}`
+    let createTask = {
+        'title': addTask,
+        'date': date,
+        'isDone': false
+    }
+    tasks.push(createTask);
+    readTask()
+    popup_add.style.display = 'none'
+})
+/* _________________ Function Create Task ___________________ */
+
+/* _________________ Function Delete Task ___________________ */
+function deleteTask(index) {
+    list.innerHTML = ''
+    let task = tasks[index]
+    let isConfirmed = confirm('Are You Sure Delete this Task: ' + task.title)
+    if(isConfirmed) {
+        tasks.splice(index, 1);
+        readTask()
+    } else {
+        readTask()
+    }
+}
+/* _________________ Function Delete Task ___________________ */
+/* _________________ Function Update Task ___________________ */
+let edit = document.querySelector('#edit');
+
+function openPopupUpdate(index) {
+    popup_update.style.display = 'block'
+    let editTask = document.querySelector('.updateTask');
+    let task = tasks[index]
+    editTask.setAttribute('value', task.title)
+
+    edit.addEventListener('click', function() {
+        list.innerHTML = ''
+        task.title = editTask.value
+        readTask()
+        popup_update.style.display = 'none'
+    })
+}
+/* _________________ Function Update Task ___________________ */
