e41fc0432ff8bff8f5a9d2f86fa5b99b2898bd81
mazenIsmeal <mazenIsmeal309@gmail.com>
2024-10-21 17:37:59 +0300

    init

--- OOP/index.js
+++ OOP/index.js
@@ -1,11 +1,13 @@
 /*__________________OOP_________________ */
 
 class User {
-    constructor(id, name, salary) {
+    #p;
+    constructor(id, name, salary, password) {
         // property
         this.i = id;
         this.n = name || 'Unknown';
         this.s = salary < 6000 ? salary + 1000 : salary;
+        this.#p = password
         this.msg = function() {
             return `Hello my name is ${this.n} and my salary is ${this.s}`
         }
@@ -14,14 +16,30 @@ class User {
     writeMsg() {
         return `Hello my name is ${this.n} and my salary is ${this.s}`
     }
+    readPass() {
+        return `my PassWord is : ${this.#p}`
+    }
 }
 
-let userOne = new User(100, 'mazen', 8000);
+let userOne = new User(100, 'mazen', 8000, '123456');
 let userTwo = new User(100, '', 5000);
 console.log(userOne);
 console.log(userTwo.msg());
 console.log(userTwo.writeMsg());
+console.log(userOne.readPass());
 
+class Admin extends User {
+    constructor(id, name, salary, eSalary, password) {
+        super(id, name, salary, password),
+        this.e = eSalary
+    }
+}
+
+let adminOne = new Admin(101, 'mazen', 15000, 5000, '789456')
+console.log(adminOne.e);
+console.log(adminOne.msg());
+console.log(adminOne.n);
+console.log(adminOne.readPass());
 
 
 
